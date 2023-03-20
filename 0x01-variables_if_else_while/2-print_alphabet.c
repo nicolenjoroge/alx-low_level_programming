402733bd585883204e9a9a4c909c8c2f33b47698
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-20 02:40:04 -0700

    for loop

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ *Return: will be 0
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
+
