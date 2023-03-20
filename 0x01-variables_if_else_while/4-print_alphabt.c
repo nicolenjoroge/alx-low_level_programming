b545723b09d958112443ce461da36aac08406304
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-20 02:57:12 -0700

    if and for

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: will be 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
