de88df3b8978f5f62048644c5109ed3f35e5630a
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-22 02:34:07 -0700

    functions

---
+++ 0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -entry
+ *
+ * Return: 0 when successful
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -entry
+ *
+ * Return: will be 0
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
