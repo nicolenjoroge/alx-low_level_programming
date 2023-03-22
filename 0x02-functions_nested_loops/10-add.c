bc3d9100c196dd02666c537001c4e4fa8a344fd6
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-22 12:48:06 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add -entry
+ *
+ * @a: para
+ *
+ * @b: para
+ *
+ * Return:: sucess
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * times_table -entry
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	
+}
