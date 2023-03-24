81478ec19ea04acea39ec3319edafc648e82f443
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-24 06:44:49 -0700

    prime

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -6,9 +6,9 @@
  */
 int main(void)
 {
-	unsigned long int i, n= 612852475143;
+	unsigned long int i, n = 612852475143;
 
-	for (i = 3; i < 782849; i= i + 2)
+	for (i = 3; i < 782849; i = i + 2)
 	{
 		while ((n % i == 0) && (n != i))
 			n = n / i;
