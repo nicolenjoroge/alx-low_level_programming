f3dcbc0aafe7ee9372648399679a77b55595108a
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-30 11:04:27 -0700

    repeat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat-entry
+ * @dest: p
+ * @src: p
+ * @n: p
+ * Return: 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	{
+		dest_len++;
+	}
+
+	for (index = 0; src[index] && index < n; index++)
+	{
+		dest[dest_len++] = src[index];
+	}
+
+	return (dest);
+}
+
+
