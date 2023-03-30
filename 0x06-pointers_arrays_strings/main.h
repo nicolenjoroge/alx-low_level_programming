e1749a3c9bd15eb73a24926304ad01f99e6fe770
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-30 12:27:03 -0700

    1337

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,7 +11,7 @@ char *cap_string(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
-
+char *leet(char *);
 
 
 #endif
