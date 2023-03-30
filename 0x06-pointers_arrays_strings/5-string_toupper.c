06dd45bd26197f586cd4c8dea7ffe99fec478c86
nicolenjoroge <nixiepatwelve@gmail.com>
2023-03-30 11:36:34 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - entry
+ * @str: p
+ * Return: 0
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+
+		index++;
+	}
+	return (str);
+}
+
