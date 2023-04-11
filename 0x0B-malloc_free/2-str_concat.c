3b7690149b3db2bf045893caefa07748265a3031
nicolenjoroge <nixiepatwelve@gmail.com>
2023-04-11 09:57:09 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,10 +10,12 @@ char *str_concat(char *s1, char *s2)
 {
 	char *conct;
 	int i, ci;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
+
 	i = ci = 0;
 
 	while (s1[i] != '\0')
@@ -23,6 +25,7 @@ char *str_concat(char *s1, char *s2)
 	conct = malloc(sizeof(char) * (i + ci + 1));
 	if (conct == NULL)
 		return (NULL);
+
 	i = ci = 0;
 
 	while (s1[i] != '\0')
@@ -36,6 +39,6 @@ char *str_concat(char *s1, char *s2)
 		i++, ci++;
 	}
 	conct[i] = '\0';
-	return (conct);	
+	return (conct);
 }
 
